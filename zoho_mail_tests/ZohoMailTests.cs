e7995b94b72a9f563f7638cf7cb4373ed13a3f86
fernandopbarboza <fernando.p.barboza@gmail.com>
2024-11-21 14:24:23 -0300

    Wait Log; Menu Novo Contato Click

--- zoho_mail_tests/ZohoMailTests.cs
+++ zoho_mail_tests/ZohoMailTests.cs
@@ -1,36 +1,60 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace zoho_mail_tests;
 
     public class ZohoMailTests
     {   
-        public static string BaseUrl = "https://accounts.zoho.com/signin";
+        public static string BaseUrl = "https://mail.zoho.com/zm/#mail/folder/inbox";
         public const int Timeout = 10;
+
         [Test]
         [Category("Selenium")]
-
         public void LoginZoho()
         {
-            IWebDriver driver = new ChromeDriver();
+        /*
+        cd C:\Program Files\Google\Chrome\Application
+        chrome.exe --remote-debugging-port=4444 --user-data-dir="C:\Users\ferna\AppData\Local\Google\Chrome\User Data"
+        */
 
+            //IWebDriver driver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+            options.AddLocalStatePreference("debuggerAddress", "127.0.0.1:5656");
+            ChromeDriver driver = new ChromeDriver(options); 
+            
             driver.Navigate().GoToUrl(BaseUrl);
-            
-            //E-mail => login_id
-            WebElement email = driver.FindElement(By.Id("login_id"));
-            email.SendKeys("contato@expertfiscaltributario.com.br");
 
-            //Senha => login_password
-            WebElement senha = driver.FindElement(By.Id("login_password"));
-            senha.SendKeys("Fernanda123");
+            WebDriverWait wait = new WebDriverWait(driver,TimeSpan.FromSeconds(120));
+            wait.Until(drv=> drv.FindElement(By.Id("folderbutton-:rf:")));
 
-            //Botão => login_submit
-            WebElement botao = driver.FindElement(By.Id("login_submit"));
-            botao.Click();
+            //Assert.That(driver.Title, Is.EqualTo("Caixa de entrada - Zoho Mail (contato@expertfiscaltributario.com.br)"));
+
+            //More Options Button => menubutton-:r0:
+            driver.FindElement(By.Id("menubutton-:r0:")).Click();            
+
+            wait = new WebDriverWait(driver,TimeSpan.FromSeconds(30));
+
+            //Novo Contato => ul.x4-list-plain li:nth-child(3)            
+            wait.Until(drv=> drv.FindElement(By.CssSelector("ul.zmlist__1a1q60m li:nth-child(4)"))).Click();
+
+            //Nome => first_name
+            wait.Until(drv=> drv.FindElement(By.Name("first_name")));
+            IWebElement nome = driver.FindElement(By.Name("first_name"));
+            nome.SendKeys("Fernando");  
+
+            //Email => email_id
+            wait.Until(drv=> drv.FindElement(By.Name("email_id")));
+            IWebElement email = driver.FindElement(By.Name("email_id"));
+            email.SendKeys("teste@expertfiscaltributario.com.br");
+
+            //Salvar => <span class="sel">Salvar</span>
+            wait.Until(drv=> drv.FindElement(By.CssSelector("span.sel")));
+            IWebElement salvar = driver.FindElement(By.CssSelector("span.sel"));
+            salvar.Click();
             
-            //Assert.That(driver.Title, Is.EqualTo("Zoho Mail (contato@expertfiscaltributario.com.br)"));
-            Assert.That(driver.Title, Is.EqualTo("Email profissional | Email corporativo seguro para sua empresa - Zoho Mail"));
+            //Assert.That(driver.Title, Is.EqualTo("Email profissional | Email corporativo seguro para sua empresa - Zoho Mail"));
 
             driver.Quit();
         }
--- zoho_mail_tests/obj/Debug/net8.0/zoho_mail_tests.AssemblyInfo.cs
+++ zoho_mail_tests/obj/Debug/net8.0/zoho_mail_tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("zoho_mail_tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3345ad4966704299854ff586adbed161a3419ad3")]
 [assembly: System.Reflection.AssemblyProductAttribute("zoho_mail_tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("zoho_mail_tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
